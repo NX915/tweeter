db32ffdbfa159e7fb2344cda792f839aa1a78c6f
Sean Ni <nixiang915@hotmail.com>
2020-09-11 00:53:44 +0000

    separate new tweet toggle script to its own file

---
+++ public/scripts/new-tweet-toggle.js
@@ -0,0 +1,23 @@
+const newTweetToggle = function() {
+  $('#tweet-toggle').on('click', function(event) {
+      
+    const tweetDiv = $('.new-tweet');
+    const icon = $('.tweet-toggle svg');
+  
+    event.preventDefault();
+    if (tweetDiv.hasClass('hidden')) {
+      tweetDiv.slideDown();
+      tweetDiv.removeClass('hidden');
+      icon.removeClass('hidden');
+      window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    } else {
+      tweetDiv.slideUp();
+      tweetDiv.addClass('hidden');
+      icon.addClass('hidden');
+    }
+  });
+};
+
+$(document).ready(() => {
+  newTweetToggle();
+});
