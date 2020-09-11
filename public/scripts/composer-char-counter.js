20ee3a4d862c26c3c292e262d64c8539bf412425
Sean Ni <nixiang915@hotmail.com>
2020-09-11 02:04:44 +0000

    character count function refactored to allow multiple event listener to be attached, character count now update after any form submit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,22 @@
 // counter that add and remove class to a counter element if user input goes over 140 characters
+const charCounter = function() {
+  const textInput = $('form').children("textarea").val();
+  const counter = $('form').find("output");
+  const count = 140 - textInput.length;
+
+  if (count < 0) {
+    $(counter).addClass("over-limit");
+  } else {
+    $(counter).removeClass("over-limit");
+  }
+  counter.html(count);
+};
+
 $(document).ready(function() {
   $(".new-tweet form").on('input', function() {
-    const textInput = $(this).children("textarea").val();
-    const counter = $(this).find("output");
-    const count = 140 - textInput.length;
-
-    if (count < 0) {
-      $(counter).addClass("over-limit");
-    } else {
-      $(counter).removeClass("over-limit");
-    }
-    counter.html(count);
+    charCounter();
+  });
+  $(".new-tweet form").on('submit', function() {
+    charCounter();
   });
 });
